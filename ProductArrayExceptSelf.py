ba77f53259fd32ef208ace0fce2bd68cf827102f
felipe266 <felipexfe34@gmail.com>
2026-07-04 19:20:56 -0300

    teste no nvim

--- ProductArrayExceptSelf.py
+++ ProductArrayExceptSelf.py
@@ -1,20 +1,22 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
         tam = len(nums)
-        prefixo = [1]*tam
-        for n in range(1,tam):
-            prefixo[n] = prefixo[n-1] * nums[n-1]
-        
-        sufixo = [1]*tam
-        for n in range(tam-2,-1,-1):
-            sufixo[n] = sufixo[n+1]*nums[n+1]
-        
-        answer = [1]*tam
+        prefixo = [1] * tam
+        for n in range(1, tam):
+            prefixo[n] = prefixo[n - 1] * nums[n - 1]
+
+        sufixo = [1] * tam
+        for n in range(tam - 2, -1, -1):
+            sufixo[n] = sufixo[n + 1] * nums[n + 1]
+
+        answer = [1] * tam
         for n in range(tam):
-            answer[n] = prefixo[n]*sufixo[n]
-        
+            answer[n] = prefixo[n] * sufixo[n]
+
         return answer
 
 
-S = Solution.productExceptSelf(Solution, [-1,1,0,-3,3])
-print(S)
+s = Solution()
+s.productExceptSelf([-1, 1, 0, -3, 3])
+print(s)
+
